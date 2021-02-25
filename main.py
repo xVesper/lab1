89f7f68c5bc82b5cd6b045f39405cc49d21aabbe
xVesper <florian.jasiuczenia@gmail.com>
2021-02-25 10:30:51 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from math import *
+napis = "pierwszy\n napis"
+
+print(napis)
+
+a = 10
+b = 5
+c = 2
+
+dzielenie = a/c
+dzielenie_calkowite = b//c
+reszta = b%c
+
+potega = a**c
+
+print(dzielenie)
+print(dzielenie_calkowite)
+print(reszta)
+print(potega)
+
+a += 2
+
+print(sqrt(25))
+print(pi)
+
+print(u"światło")
+
+print("wynika dzialania jest rowny a=%(zm)d" %{"zm": 12})
+
+napis1 = "informatyka ogolna"
+x = 2
+
+print("specjalizacja %s grupa nr %d" % (napis1,x))
+
